563c75b14e5ce738f474b92b4c78fc9430c7bd3f
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-07-28 01:24:40 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,6 +3,14 @@ import css from './Modal.module.css';
 import { createPortal } from 'react-dom';
 const modalRoot = document.querySelector('#modal-root');
 export class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', e => {
+      console.log(e.code);
+      if (e.key === 'Escape') {
+        this.props.toggleModal();
+      }
+    });
+  }
   render() {
     return createPortal(
       <div className={css.overlay} onClick={this.props.toggleModal}>
